747f60ea52378781ab95c7c0f876683cc5365c5c
tomsonas3000 <tomasjorudas23@gmail.com>
2021-11-22 20:34:42 +0200

    fix namespaces

--- restaurant-manager-tests/OrdersTests.cs
+++ restaurant-manager-tests/OrdersTests.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using restaurant_manager_visma;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using restaurant_manager_visma;
 
-namespace restaurant_manager_visma.Tests
+namespace restaurant_manager_tests
 {
     [TestClass()]
     public class OrdersTests
